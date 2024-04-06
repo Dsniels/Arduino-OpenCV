87e3ddf5c4f0330c56564744cde82fab64322126
Daniel Salazar <dsnile.salazr@gmail.com>
2024-04-05 23:08:21 -0600

    Add neural network code for temperature conversion

---
+++ Python/RedNeuronal.py
@@ -0,0 +1,32 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+oculta1 = tf.keras.layers.Dense(units=3, input_shape=[1])
+oculta2 = tf.keras.layers.Dense(units=3)
+salida = tf.keras.layers.Dense(units=1)
+modelo = tf.keras.Sequential([oculta1, oculta2, salida])
+modelo.compile(
+    optimizer=tf.keras.optimizers.Adam(0.1),
+    loss='mean_squared_error'
+)
+print("Comenzando entrenamiento...")
+historial = modelo.fit(celsius, fahrenheit, epochs=1000, verbose=False)
+print("Modelo entrenado!")
+
+plt.xlabel("# Epoca")
+plt.ylabel("Magnitud de pérdida")
+plt.plot(historial.history["loss"])
+
+print("Hagamos una predicción!")
+resultado = modelo.predict(np.array([100.0]))
+print("El resultado es " + str(resultado) + " fahrenheit!")
+
+
+
+print("Variables internas del modelo")
+#print(capa.get_weights())
+print(oculta1.get_weights())
+print(oculta2.get_weights())
+print(salida.get_weights())
